7aa237eb8db87dc5136e9d42ddbe9f172cb390a8
OLGA DOBROVOLSKA <3250186@gmail.com>
2022-11-08 13:40:12 +0200

    adding the solution for the 04-express-quest

--- userHandlers.js
+++ userHandlers.js
@@ -44,8 +44,30 @@ const postUser = (req, res) => {
       res.status(500).send("Error saving the User");
     });
 };
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+  database
+    .query(
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500).send("Error updating the user");
+    });
+};
+
 module.exports = {
   getUser,
   getUserById,
   postUser,
+  updateUser,
 };
